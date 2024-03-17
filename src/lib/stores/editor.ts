66e2002e5c4754ee3394b37705dadb7ec784e4c8
Vedant Chainani <vedantchainani1084@gmail.com>
2024-03-17 14:49:19 +0530

    chore: add editor store

--- src/lib/stores/editor.ts
+++ src/lib/stores/editor.ts
@@ -3,6 +3,9 @@ import { create } from 'zustand';
 import type * as monaco from 'monaco-editor';
 
 type State = {
+  isCreating: 'file' | 'folder' | null;
+  name: string;
+  parentFolder: string;
   monaco: monaco.editor.IStandaloneCodeEditor | null;
   code: string;
 };
@@ -10,11 +13,20 @@ type State = {
 interface Actions {
   setMonaco: (monaco: monaco.editor.IStandaloneCodeEditor) => void;
   setCode: (code: string) => void;
+  setIsCreating: (type: 'file' | 'folder' | null) => void;
+  setParentFolder: (parentFolder: string) => void;
+  setName: (name: string) => void;
 }
 
 export const useEditor = create<State & Actions>((set) => ({
   monaco: null,
   code: '',
+  isCreating: null,
+  parentFolder: '/',
+  name: '',
   setCode: (code) => set({ code }),
   setMonaco: (monaco) => set({ monaco }),
+  setIsCreating: (isCreating) => set({ isCreating }),
+  setParentFolder: (parentFolder) => set({ parentFolder }),
+  setName: (name) => set({ name }),
 }));
