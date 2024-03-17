87fc91f971af0d2cd020b92541f5807dedb58f8a
Vedant Chainani <vedantchainani1084@gmail.com>
2024-03-17 18:49:35 +0530

    chore: add default page

---
+++ src/components/panels/main-panel/DefaultPage.tsx
@@ -0,0 +1,16 @@
+import { AOS_ASCII } from '~/lib/helpers/editor';
+
+const DefaultPage = () => {
+  console.log(AOS_ASCII);
+  return (
+    <div className='flex h-full items-center justify-center p-12'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <pre className='select-none text-xs dark:text-neutral-300'>
+          {AOS_ASCII}
+        </pre>
+      </div>
+    </div>
+  );
+};
+
+export default DefaultPage;
